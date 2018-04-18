a6d7f831b9fc61a77e7c91d8a6bd89cf31f0153d
ingerulro <elektryon.dev@gmail.com>
2018-04-18 22:16:39 +0300

    Typo

--- deployment/deploy_solace.bash
+++ deployment/deploy_solace.bash
@@ -14,7 +14,7 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get -y upgrade
 sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password $ROOT_SQL_PASS"
 sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $ROOT_SQL_PASS"
 echo -e "[client]\nuser=root\npassword=$ROOT_SQL_PASS" | sudo tee /root/.my.cnf
-sudo DEBIAN_FRONTEND=noninteractive apt-get -y install git python-virtualenv python3-virtualenv curl ntp build-essential screen cmake pkg-config libboost-all-dev libevent-dev libunbound-dev libminiupnpc-dev libunwind8-dev liblzma-dev libldns-dev libexpat1-dev libgtest-dev mysql-server lmdb-utils libzmq3-dev doxygen graphviz
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y install git python-virtualenv python3-virtualenv curl ntp build-essential screen cmake pkg-config libboost-all-dev libevent-dev libunbound-dev libminiupnpc-dev libldns-dev libexpat1-dev libgtest-dev mysql-server lmdb-utils doxygen graphviz
 cd ~
 git clone https://github.com/Infinity-Pools/nodejs.git nodejs-pool # Change this depending on how the deployment goes.
 cd /usr/src/gtest
@@ -26,12 +26,12 @@ sudo systemctl enable ntp
 cd /usr/local/src
 sudo git clone https://github.com/schmeckles22/SolaceCoin.git solace
 cd solace
-sudo make -j$(nproc)
+sudo make release-static -j$(nproc)
 sudo cp ~/nodejs-pool/deployment/services/solace.service /lib/systemd/system/
 sudo useradd -m solacedaemon -d /home/solacedaemon
 BLOCKCHAIN_DOWNLOAD_DIR=$(sudo -u solacedaemon mktemp -d)
 sudo -u solacedaemon wget --limit-rate=50m -O $BLOCKCHAIN_DOWNLOAD_DIR/blockchain.raw https://infinity-pools.cc/solace/blockchain.raw
-sudo -u solacedaemon /usr/local/src/solace/build/release/bin/solace-blockchain-import --input-file $BLOCKCHAIN_DOWNLOAD_DIR/blockchain.raw --verify 0 --data-dir /home/solacedaemon/.solace
+sudo -u solacedaemon /usr/local/src/solace/build/release/bin/solace-blockchain-import --input-file $BLOCKCHAIN_DOWNLOAD_DIR/blockchain.raw --verify 1 --data-dir /home/solacedaemon/.solace
 sudo -u solacedaemon rm -rf $BLOCKCHAIN_DOWNLOAD_DIR
 sudo systemctl daemon-reload
 sudo systemctl enable solace
